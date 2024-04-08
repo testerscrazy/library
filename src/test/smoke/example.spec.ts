3086b9772dd8dc733c44a8646e450820bcad1f08
CodeCraftsWoman <146996229+CodeCraftsWoman@users.noreply.github.com>
2024-04-08 14:28:51 -0700

    Kan 11-Framework Structure  (#2)
    * KAN-6 add space
    
    * KAN-6 added another space
    
    * KAN-6 just added one more space
    
    * KAN-6 removed all spaces
    
    * KAN-11 framework structure
    
    ---------
    
    Co-authored-by: Jwan Khasraw <jwan_khasraw@yahoo.com>

--- src/test/smoke/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('basic test', async ({ page }) => {
-  await page.goto('http://example.com');
-  const pageTitle = await page.title();
-  expect(pageTitle).toBe('Example Domain');
-});
