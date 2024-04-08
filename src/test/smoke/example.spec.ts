57e24d0a72499c14e3898686f627414fb4497ede
Jwan Khasraw <jwan_khasraw@yahoo.com>
2024-04-08 12:03:31 -0700

    KAN-6 removed all spaces

--- src/test/smoke/example.spec.ts
+++ src/test/smoke/example.spec.ts
@@ -4,7 +4,4 @@ test('basic test', async ({ page }) => {
   await page.goto('http://example.com');
   const pageTitle = await page.title();
   expect(pageTitle).toBe('Example Domain');
-
-
-  
-});
+  });
