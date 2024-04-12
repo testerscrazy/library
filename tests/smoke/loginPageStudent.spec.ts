416913f6be01f0931f5eb5942e395ef792f472ba
testerscrazy <165315196+testerscrazy@users.noreply.github.com>
2024-04-12 14:13:18 -0700

    KAN-17 page title verified (#8)
    * KAN-17 page title verified
    
    * KAN-17 updated
    
    * Updated conflict
    
    * KAN-17 Updated

---
+++ tests/smoke/loginPageStudent.spec.ts
@@ -0,0 +1,28 @@
+import { expect, test } from "playwright/test";
+import { LoginPage } from "../../pages/loginPage";
+const { chromium } = require('playwright-core');
+
+let browser;
+let loginPage: LoginPage;
+
+
+test.beforeAll(async() =>{
+    // Initialize browser
+    browser = await chromium.launch();
+})
+
+
+test.beforeEach(async({page})=>{
+    // go to base url
+    loginPage = new LoginPage(page);
+    await loginPage.gotoLoginPage();
+})
+
+
+test("As a Student I verify that the library login page exist", async ({ page }) => {
+
+    loginPage = new LoginPage(page);
+    await expect(page.url()).toEqual(process.env.library_url);
+
+})
+
